f411bf4e19e699b299ec9b34378028954634f54e
Mohsin Miah <moemia12@gmail.com>
2020-12-30 23:57:42 +0000

    jQuery update

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const 
+$(document).ready(function(){
+    $('.right-arrow').on('click', function(){
+        let currentImg = $('#active');
+        let nextImg = currentImg.next();
+
+        if(currentImgg){
+
+        }
+    })
+})
