3e816a2af78c6062534a6ec215671dc35cc66808
Mohsin Miah <moemia12@gmail.com>
2020-12-31 16:51:36 +0000

    Update

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-$(document).ready(function(){
-    $('.right-arrow').on('click', function(){
-        let currentImg = $('#active');
-        let nextImg = currentImg.next();
+const rightArrow = document.getElementById('right-arrow');
 
-        if(currentImgg){
+// Handle right click
 
-        }
-    })
-})
+rightArrow.onclick = () =>{
+    const activeImg = document.querySelector(i.active)
+    const nextImg = activeImg.nextElementSibling;
+
+    if(activeImg && nextImg){
+        activeImg.classList.remove('active');
+        nextImg.classList.add('active');
+    }
+};
