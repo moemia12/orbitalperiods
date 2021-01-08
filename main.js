ebb8aaba66c2c08b8f341801ff115da3e8cd7d65
Mohsin Miah <moemia12@gmail.com>
2021-01-08 22:22:47 +0000

    jQuery Update

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-const rightArrow = document.getElementById('right-arrow');
-const activeImg = document.querySelector(".active")
-const nextImg = activeImg.nextElementSibling;
+let rightArrow = document.getElementById('right-arrow');
+let activeImg = document.querySelector(".active");
+let nextImg = activeImg.nextElementSibling;
 
 
 // Handle left click
 
 rightArrow.addEventListener('click', function(){  //Left arrow button clicked
     
-    activeImg.classList.remove("active");       //
-    nextImg.classList.add("active");
+
+   activeImg.classList.remove("active");       //
+   nextImg.classList.add("active");
     
     return nextImg
-    
 });
+
+$(document).ready(function(){
+    $(".right-arrow").on('click', function(){
+        $('.active').removeClass('active');
+        $('.not-active').addClass('active')
+    })
+})
