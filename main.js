2021bc76d79430df0c945282d291deefbb7dc2d6
Mohsin Miah <moemia12@gmail.com>
2021-01-10 15:29:48 +0000

    JS Update

--- main.js
+++ main.js
@@ -1,29 +1,22 @@
-var icons = [...document.querySelectorAll('.icon-container .fa')];
+// Declaring Earth, Icon and Answer Container variables
+let earth = 7917.5;               
+let icons = [...document.querySelectorAll('.icon-container .fa')];
+let answerContainer = document.getElementById("answer-container");
 
-function adjustActive (adjustment) {
-  var current = icons.find(it => it.id === 'active');
+//Function to slide icons
+function adjustActive (adjustment) { 
+  var current = icons.find(it => it.id === 'active'); 
   var currentIndex = icons.indexOf(current);
   var nextIndex = (currentIndex + adjustment) % icons.length;
-  
+
   if (nextIndex < 0) nextIndex = icons.length - 1;
-  
+  console.log(current);
   current.removeAttribute('id');
-  icons[nextIndex].id = 'active';
+  icons[nextIndex].id = 'active'; 
+  let orbitalPeriod = earth / current.getAttribute("value"); 
+  answerContainer.innerHTML = orbitalPeriod
+ 
 }
 
 document.querySelector('#left-arrow').addEventListener('click', e => adjustActive(-1));
 document.querySelector('#right-arrow').addEventListener('click', e => adjustActive(1));
-
-
-let earth = 7917.5
-let answerContainer = document.getElementById("answer-container")
-
-let findOrbitalPeriod = () =>{
-    if(document.getElementById('right-arrow').clicked == true){
-        let orbitalPeriod = earth/ current; 
-        return answerContainer.innerHTML(orbitalPeriod.value)
-    }
- }
-
-
-
