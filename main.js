bc93d8b4ff62907850942ae7bb37cdd3eb109a34
Mohsin Miah <moemia12@gmail.com>
2021-01-18 04:01:36 +0000

    Planet Slide update

--- main.js
+++ main.js
@@ -3,48 +3,45 @@
 let icons = [...document.querySelectorAll('.icon-container .fa')];
 let answerContainer = document.getElementById("answer-container");
 let answerDiv = document.getElementById("answer-div");
+let planetImgs = [...document.querySelectorAll(".planet-container img")];
+let currentPlanet = document.getElementById("active-planet");
 
-//Function to slide icons
-function adjustActive (adjustment) { 
-  var current = icons.find(it => it.id === 'active'); 
-  var currentIndex = icons.indexOf(current);
+    //Function to slide icons
+     function adjustActive (adjustment) { 
+     var current = icons.find(it => it.id === 'active'); 
+     var currentIndex = icons.indexOf(current);
+     var nextIndex = (currentIndex + adjustment) % icons.length;
+
+     if (nextIndex < 0) nextIndex = icons.length - 1;
+    current.removeAttribute('id');
+    icons[nextIndex].id = 'active'; 
+
+    // Action to find Orbital Period
+    let planet = currentPlanet.dataset.radius;  
+    let nextElement = document.getElementById("active");
+    let nextElements = nextElement.getAttribute("value")
+    let orbitalPeriod = planet / nextElements;
+
+    answerDiv.innerHTML = orbitalPeriod.toFixed(2);
+
+    }
+
+  // Function to slide icons
+
+  function adjustActivePlanet (adjustment) { 
+  var current = planetImgs.find(it => it.id === 'active-planet'); 
+  var currentIndex = planetImgs.indexOf(current);
   var nextIndex = (currentIndex + adjustment) % icons.length;
 
-  if (nextIndex < 0) nextIndex = icons.length - 1;
+  if (nextIndex < 0) nextIndex = planetImgs.length - 1;
   current.removeAttribute('id');
-  icons[nextIndex].id = 'active'; 
+  planetImgs[nextIndex].id = 'active-planet'; 
 
-// Action to find Orbital Period
-  let earth = 7917.5;  
-  let nextElement = document.getElementById("active");
-  let nextElements = nextElement.getAttribute("value")
-  let orbitalPeriod = earth / nextElements;
+  }
 
-  answerDiv.innerHTML = orbitalPeriod.toFixed(2);
 
-}
 
 document.querySelector('#left-arrow').addEventListener('click', e => adjustActive(-1));
 document.querySelector('#right-arrow').addEventListener('click', e => adjustActive(1));
-
-// Function to create Planets Container
-
-function adjustActiv (adjustment) { 
-  var current = icons.find(it => it.id === 'active'); 
-  var currentIndex = icons.indexOf(current);
-  var nextIndex = (currentIndex + adjustment) % icons.length;
-
-  if (nextIndex < 0) nextIndex = icons.length - 1;
-  current.removeAttribute('id');
-  icons[nextIndex].id = 'active'; }
-
-  // Function to create Planets Container
-
-function adjustActiv (adjustment) { 
-  var current = icons.find(it => it.id === 'active'); 
-  var currentIndex = icons.indexOf(current);
-  var nextIndex = (currentIndex + adjustment) % icons.length;
-
-  if (nextIndex < 0) nextIndex = icons.length - 1;
-  current.removeAttribute('id');
-  icons[nextIndex].id = 'active'; }
+document.querySelector('#left-arrow-planet').addEventListener('click', e => adjustActivePlanet(-1));
+document.querySelector('#right-arrow-planet').addEventListener('click', e => adjustActivePlanet(1));
